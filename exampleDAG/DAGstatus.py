e6501109387fe534cb20c531ea83556fb3f461bb
Robin Aggleton <robin.aggleton@cern.ch>
2015-08-28 15:33:13 +0100

    Rename 'staus.py' to 'DAGstatus.py' as more sensible. Also add in colours to output, and nodes runnign in summary.

--- exampleDAG/DAGstatus.py
+++ exampleDAG/DAGstatus.py
@@ -2,19 +2,15 @@
 """
 Code to interpret a DAGman status output, and present it in a more user-friendly manner.
 
-Make sure you have NODE_STATUS_FILE set in your DAD job file.
-
-USAGE:
-
-./status.py <name of DAG status file>
-
 TODO:
 - maybe use namedtuples instead of full-blown classes?
-- use argparse
 """
 
+
+import argparse
 import sys
 import logging
+from pprint import pprint
 import re
 import os
 from collections import OrderedDict
@@ -24,19 +20,55 @@ logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def strip_doublequotes(string):
-    return re.search(r'\"(.*)\"', string).group(1)
+def strip_doublequotes(line):
+    return re.search(r'\"(.*)\"', line).group(1)
+
+
+class bcolors:
+    """For terminal coloured output"""
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    @staticmethod
+    def color(state):
+        """Return color code based on state"""
+        if state.startswith("STATUS_ERROR"):
+            return bcolors.FAIL
+        elif state.startswith("STATUS_SUBMITTED"):
+            return bcolors.OKBLUE
+        elif state.startswith("STATUS_DONE"):
+            return bcolors.OKGREEN
+        else:
+            return bcolors.ENDC
 
 
 class ClassAd(object):
+    """Base class for ClassAds."""
     def __init__(self):
         pass
 
 
 class DagStatus(ClassAd):
-    def __init__(self, timestamp, dag_status, nodes_total, nodes_done,
-                 nodes_pre, nodes_queued, nodes_post, nodes_ready,
-                 nodes_unready, nodes_failed, job_procs_held, job_procs_idle):
+    """Class to describe status of DAG as a whole."""
+    def __init__(self,
+                 timestamp,
+                 dag_status,
+                 nodes_total,
+                 nodes_done,
+                 nodes_pre,
+                 nodes_queued,
+                 nodes_post,
+                 nodes_ready,
+                 nodes_unready,
+                 nodes_failed,
+                 job_procs_held,
+                 job_procs_idle):
         self.timestamp = timestamp
         self.dag_status = strip_doublequotes(dag_status)
         self.nodes_total = int(nodes_total)
@@ -50,27 +82,39 @@ class DagStatus(ClassAd):
         self.job_procs_held = int(job_procs_held)
         self.job_procs_idle = int(job_procs_idle)
         self.nodes_done_percent = round(100. * self.nodes_done / self.nodes_total)
+        self.job_procs_running = self.nodes_total - self.job_procs_held - self.nodes_queued - self.nodes_done - self.nodes_failed
 
 
 class NodeStatus(ClassAd):
-    def __init__(self, node, node_status, status_details, retry_count,
-                 job_procs_queued, job_procs_held):
+    """Class to describe state of individual job node in the DAG."""
+    def __init__(self,
+                 node,
+                 node_status,
+                 status_details,
+                 retry_count,
+                 job_procs_queued,
+                 job_procs_held):
         self.node = strip_doublequotes(node)
         self.node_status = strip_doublequotes(node_status)
-        self.status_details = status_details
+        self.status_details = status_details.replace('"', '')
         self.retry_count = int(retry_count)
         self.job_procs_queued = int(job_procs_queued)
         self.job_procs_held = int(job_procs_held)
 
 
 class StatusEnd(ClassAd):
-    def __init__(self, end_time, next_update):
+    """Class to describe state of resporting."""
+    def __init__(self,
+                 end_time,
+                 next_update):
         self.end_time = strip_doublequotes(end_time)
         self.next_update = strip_doublequotes(next_update)
 
 
-def process(status_filename):
+def process(status_filename, summary):
     """Main function to process the status file"""
+
+    print status_filename, ":"
 
     dag_status = None
     node_statuses = []
@@ -124,10 +168,10 @@ def process(status_filename):
                 parts = line.split(" = ")
                 contents[parts[0]] = parts[1]
 
-    print_table(dag_status, node_statuses, status_end)
+    print_table(dag_status, node_statuses, status_end, summary)
 
 
-def print_table(dag_status, node_statuses, status_end):
+def print_table(dag_status, node_statuses, status_end, summary):
     """Print a pretty-ish table with important info"""
     # Here we auto-create the formatting strings for each row,
     # and auto-size each column based on max size of contents
@@ -136,9 +180,10 @@ def print_table(dag_status, node_statuses, status_end):
     job_dict = OrderedDict()  # holds column title as key and object attribute name as value
     job_dict["Node"] = "node"
     job_dict["Status"] = "node_status"
+    job_dict["Detail"] = "status_details"
     job_dict["Retries"] = "retry_count"
-    # Auto-size each column
-    job_col_widths = [max(max(len(str(x.__dict__[v])) for x in node_statuses), len(k)) for k, v in job_dict.iteritems()]
+    # Auto-size each column - find maximum of column header and column contents
+    job_col_widths = [max([len(str(x.__dict__[v])) for x in node_statuses]+[len(k)]) for k, v in job_dict.iteritems()]
     # make formatter string to be used for each row, auto calculates number of columns
     job_format = "  |  ".join(["{{:<{}}}"]*len(job_dict.keys())).format(*job_col_widths)
     job_header = job_format.format(*job_dict.keys())
@@ -148,7 +193,9 @@ def print_table(dag_status, node_statuses, status_end):
     summary_dict["DAG status"] = "dag_status"
     summary_dict["Total"] = "nodes_total"
     summary_dict["Queued"] = "nodes_queued"
-    summary_dict["Ready"] = "nodes_ready"
+    summary_dict["Idle"] = "job_procs_idle"
+    # summary_dict["Ready"] = "nodes_ready"
+    summary_dict["Running"] = "job_procs_running"
     summary_dict["Failed"] = "nodes_failed"
     summary_dict["Done"] = "nodes_done"
     summary_dict["Done %"] = "nodes_done_percent"
@@ -157,7 +204,8 @@ def print_table(dag_status, node_statuses, status_end):
     summary_header = summary_format.format(*summary_dict.keys())
 
     # Now figure out how many char columns to occupy for the *** and ---
-    columns = max(len(job_header), len(summary_header))
+    columns = len(summary_header) if summary else max(len(job_header), len(summary_header))
+    columns += 1
     term_rows, term_columns = os.popen('stty size', 'r').read().split()
     term_rows = int(term_rows)
     term_columns = int(term_columns)
@@ -165,28 +213,40 @@ def print_table(dag_status, node_statuses, status_end):
         columns = term_columns
 
     # Now actually print the table
-    print "*" * columns
-    # Print info for each job.
-    print job_header
-    print "-" * columns
-    for n in node_statuses:
-        print job_format.format(*[n.__dict__[v] for v in job_dict.values()])
-    print "*" * columns
+    if not summary:
+        print "*" * columns
+        # Print info for each job.
+        print bcolors.ENDC, job_header
+        print "-" * columns
+        for n in node_statuses:
+            print (bcolors.color(n.node_status) +
+                job_format.format(*[n.__dict__[v] for v in job_dict.values()]))
+        print bcolors.ENDC + "-" * columns
     # print summary of all jobs
+    print "*" * columns
     print summary_header
     print "-" * columns
-    print summary_format.format(*[dag_status.__dict__[v] for v in summary_dict.values()])
-    print "*" * columns
-    # print time of next update
-    print "Status recorded at:", status_end.end_time
-    print "Next update:       ", status_end.next_update
-    print "*" * columns
+    # Make it coloured depending on job status
+    sum_col = bcolors.ENDC
+    # if summary:
+    print (bcolors.color(dag_status.dag_status) +
+        summary_format.format(*[dag_status.__dict__[v] for v in summary_dict.values()]))
+    if not summary:
+        print bcolors.ENDC + "-" * columns
+        # print time of next update
+        print "Status recorded at:", status_end.end_time
+        print "Next update:       ", status_end.next_update
+    print bcolors.ENDC + "*" * columns
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Usage:"
-        print "    python status.py <dag status file>"
-        exit(1)
-    else:
-        process(sys.argv[1])
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("-v", "--verbose", help="enable debugging mesages", action='store_true')
+    parser.add_argument("-s", "--summary", help="only printout very short summary of all jobs", action='store_true')
+    parser.add_argument("statusFile", help="name(s) of DAG status file(s), separated by spaces", nargs="*")
+    args = parser.parse_args()
+    if args.verbose:
+        log.setLevel(logging.DEBUG)
+    for f in args.statusFile:
+        process(f, args.summary)
